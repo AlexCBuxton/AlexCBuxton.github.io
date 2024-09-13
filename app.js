f23fcb5c39c39996326d99ae549772baecb8a8f2
AlexBuxton <alex.buxton2005@gmail.com>
2024-09-13 11:47:31 +0100

    CSS finished

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 function Q1_correct() {
-    document.getElementById("Q1_result").innerHTML = "Correct! We omitted d(x, y) = 0 if and only if x = y from the positivity property.";
+    document.getElementById("Q1_result").style.color = "green"
+    document.getElementById("Q1_result").innerHTML = "Correct! We omitted 'd(x, y) = 0 if and only if x = y' from the positivity property.";
 }
 
 function Q1_incorrect() {
+    document.getElementById("Q1_result").style.color = "red"
     document.getElementById("Q1_result").innerHTML = "Inorrect. Please try again.";
 }
 
 function submit_Q2() {
     var Q2_answer = document.getElementById("Q2_answer").value;
     if (Q2_answer == 1) {
+        document.getElementById("Q2_result").style.color = "green"
         document.getElementById("Q2_result").innerHTML = "Correct! Since 0 &#x2260; 5, d(0, 5) = 1.";
     } else {
+        document.getElementById("Q2_result").style.color = "red"
         document.getElementById("Q2_result").innerHTML = "Inorrect. Please try again.";
     }
 }
@@ -18,8 +22,10 @@ function submit_Q2() {
 function submit_Q3() {
     var Q3_answer = document.getElementById("Q3_answer").value;
     if (Q3_answer == "True") {
+        document.getElementById("Q3_result").style.color = "green"
         document.getElementById("Q3_result").innerHTML = "Correct! This is known as the Euclidean metric, and can be given by d((x&#x2080;, y&#x2080;), (x&#x2081;, y&#x2081;)) = &#x2016;(x&#x2080;, y&#x2080;) - (x&#x2081;, y&#x2081;)&#x2016;&#x2082;, where &#x2016;&#x00B7;&#x2016;&#x2082; is the Euclidean norm (i.e. the standard norm on &#x211D;&#xB2;).";
     } else {
+        document.getElementById("Q3_result").style.color = "red"
         document.getElementById("Q3_result").innerHTML = "Inorrect. Please try again.";
     }
 }
